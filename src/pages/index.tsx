db049a4f053d6fd50bf706a4ffdf5c0ad90e0d4e
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2023-08-01 21:50:13 +0200

    feat: add basic homepage with fetched posts data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,28 @@
-import { Layout } from "../components/layout";
+import {Layout} from "../components/layout";
 
-export default function Home() {
-  return (
-    <Layout>
-      content
-    </Layout>
-  )
+interface IHomeProps {
+    posts: IHomeProps;
 }
+
+const Home = (props: IHomeProps) => {
+    const {posts} = props;
+
+    return (
+        <Layout>
+            content
+        </Layout>
+    )
+}
+
+export async function getServerSideProps() {
+    const response = await fetch('http://localhost:3000/api/posts');
+    const posts = await response.json();
+
+    return {
+        props: {
+            posts
+        }
+    };
+}
+
+export default Home;
