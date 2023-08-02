02966c345af0ac857d822452bdc4fb940c6f0b2d
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2023-08-02 14:03:50 +0200

    feat: add style var and formatDate func

--- src/utils/funcs.ts
+++ src/utils/funcs.ts
@@ -1,3 +1,14 @@
 export const makeArray = (size: number): number[] => {
     return Array.from(new Array(size)).map((_, index) => index);
 }
+
+export const formatDate = (dateString: string): string => {
+    const date = new Date(dateString);
+
+    const day = date.toLocaleString('en-US', { day: '2-digit' });
+    const month = date.toLocaleString('en-US', { month: 'short' });
+    const year = date.toLocaleString('en-US', { year: 'numeric' });
+
+    return `${day} ${month} ${year}`;
+}
+
