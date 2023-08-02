275f262c41c0fd8196c0520c20d2fbf19ec653bb
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2023-08-02 13:17:40 +0200

    feat: new mocked data

--- src/data/mocked-posts.ts
+++ src/data/mocked-posts.ts
@@ -1,14 +1,25 @@
-export const mockedPosts = [
-    {
-        id: 1,
-        title: 'Post 1',
-    },
-    {
-        id: 2,
-        title: 'Post 2',
-    },
-    {
-        id: 3,
-        title: 'Post 3',
-    },
-]
+import {makeArray} from "../utils/funcs";
+import {mockedPost} from "./mocked-post";
+
+export interface IPost {
+    id: number;
+    title: string;
+    topic: string;
+    author: string;
+    date: Date;
+    shortDescription: string;
+    fullDescription: string;
+    thumbnailSrc: string,
+    imageSrc: string,
+}
+
+
+export const mockedPosts: IPost[] = [];
+
+makeArray(5).map((index) => {
+    mockedPosts.push({
+        ...mockedPost,
+        id: ++index,
+        date: new Date(2023, 8, +index),
+    });
+});
